6e61f7200cc6cc1417693b7564ac7a21abc21f85
Cris <medikacion@gmail.com>
2026-04-19 14:40:57 -0300

    fix: ultra-minimal app.js - listen immediately on PORT env var

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const http = require('http');
-const PORT = process.env.PORT || 8080;
-
-console.log('[START] PORT=' + PORT);
-
-const srv = http.createServer((req, res) => {
-  console.log('[REQ] ' + req.method + ' ' + req.url);
+require('http').createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('OK');
-});
-
-srv.on('error', (e) => {
-  console.error('[ERROR]', e.code, e.message);
-  process.exit(1);
-});
-
-srv.listen(PORT, '0.0.0.0', () => {
-  console.log('[LISTEN] OK on ' + PORT);
-});
-
-setTimeout(() => console.log('[HEALTH] Running'), 5000);
+}).listen(process.env.PORT || 8080, '0.0.0.0');
